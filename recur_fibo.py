2f0b6843797a2ec8a31758899cc0805b68aa4612
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-06-13 14:34:07 +0530

    Add files via upload

---
+++ recur_fibo.py
@@ -0,0 +1,17 @@
+# def fibonacci(n):
+#     if n in [0, 1]:
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+# n=int(input("enter number"))
+# print(fibonacci(n))
+
+# def productOfArray(arr):
+#     if (arr) == 0:
+#         return 0
+#     if (arr) == 1:
+#         return arr[0]
+#     else:
+#         return arr[(arr)-1] * productOfArray(arr(num))
+# num=int(input("enter num..."))
+# print(productOfArray(num))
